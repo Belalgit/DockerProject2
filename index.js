67df02e189bf6477235a4890f7369ffc413cf07c
Belalgit <belaluiu15@gmail.com>
2024-03-27 17:40:38 +0600

    code change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8080
 const hostname = '127.0.0.1';
 
 app.get('/', (req, res) => {
-  res.send('Hello World! This is a Docker project1 for Begineers. I love you.')
+  res.send('Hello World! This is a Docker project1 for Begineers. I love you too.')
 })
 
 app.listen(port, () => {
